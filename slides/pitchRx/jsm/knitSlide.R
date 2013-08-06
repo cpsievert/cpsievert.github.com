cdca8c152f94b3008dbed216b9935375caf03f63
Carson Sievert <cpsievert1@gmail.com>
2013-08-06 10:29:16 -0400

    jsm slides

--- slides/pitchRx/jsm/knitSlide.R
+++ slides/pitchRx/jsm/knitSlide.R
@@ -1,4 +1,5 @@
 setwd("~/Desktop/github/local/cpsievert.github.com/slides/pitchRx/jsm")
+require(knitr)
 knit('index.Rmd', envir=new.env()) #clicking 'knit HTML' in RStudio works better. Environment issue?
-system("pandoc -s -S -i -t dzslides index.md -o index.html")
-system("grep -Ev '<h1>Remove Me!</h1>' index.html > index2.html") #Remove everything that matches '<h1>Remove Me!</h1>'
+system("pandoc -s -S -i -t dzslides index.md -o index-titles.html")
+system("grep -Ev '<h1>Remove Me!</h1>' index-titles.html > index.html") #Remove everything that matches '<h1>Remove Me!</h1>'
