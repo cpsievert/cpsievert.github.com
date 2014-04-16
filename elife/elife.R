4c488278bb1c987c69d0a5546133590adf9f82bf
Anonymous@example.com <cpsievert1@gmail.com>
2014-04-15 22:12:32 -0500

    Update elife post

--- elife/elife.R
+++ elife/elife.R
@@ -1,54 +1,63 @@
+# Load/install necessary packages
+library(devtools)
+#install_github("ropensci/elife")
+#install_github("kshirley/LDAviz")
+#install_github("cpsievert/LDAvis")
 library(elife)
-# Obtain all the dois! 
-dois <- searchelife(terms="*", searchin="article_title", boolean="matches")
-# Obtain all the abstracts (and publication dates)
-abs <- NULL
-dates <- NULL
-for (i in 1:length(dois)) {
-  ab <- elife_doi(dois[i], ret = "abstract")
-  date <- elife_doi(dois[i], ret = "pub_date_date")
-  abs <- c(abs, ab)
-  dates <- c(dates, date)
-}
-
-#Preprocess the text and convert to document-term matrix
+library(LDAviz)
+library(LDAvis)
 library(topicmodels)
 library(tm)
+library(Rmpfr)
+
+# Obtain all the dois! 
+dois <- searchelife(terms="*", searchin="article_title", boolean="matches")
+
+# If you re-run this analysis, there will be more dois/articles available, which will alter results
+# For this reason, I've included the dois I used as 'dois.csv'
+# Also keep in mind that LDA estimation will not produce the same point estimates (but it should produce similar ones!)
+
+dois <- read.csv("dois.csv", stringsAsFactors = FALSE)[,1]
+
+# Obtain abstracts, subject areas, and publication dates (you really only need the abstracts)
+abs <- sapply(dois, function(x) elife_doi(x, ret = "abstract"))
+areas <- sapply(dois, function(x) elife_doi(x, ret = "subject_area"))
+dates <- sapply(dois, function(x) elife_doi(x, ret = "pub_date_date"))
+
+#Preprocess the text and convert to document-term matrix
 dtm.control <- list(
-  tolower = T,
-  removePunctuation = T,
-  removeNumbers = T,
+  tolower = TRUE,
+  removePunctuation = TRUE,
+  removeNumbers = TRUE,
   stopwords = stopwords("english"),
-  stemming = T,
-  wordLengths = c(3,Inf),
+  stemming = TRUE,
+  wordLengths = c(3, Inf),
   weighting = weightTf
 )
-
 corp <- Corpus(VectorSource(unlist(abs, use.names=FALSE)))
 dtm <- DocumentTermMatrix(corp, control = dtm.control)
 dim(dtm)
 dtm <- removeSparseTerms(dtm, 0.99)
 dim(dtm)
 
-# Drop documents with little or no text (left)
+# Drop documents with little or no text
 dtm <- dtm[rowSums(as.matrix(dtm)) > 0, ]
 
 # Fit models and find an optimal number of topics as suggested by Ben Marmick --
 # http://stackoverflow.com/questions/21355156/topic-models-cross-validation-with-loglikelihood-or-perplexity/21394092#21394092
-
-harmonicMean <- function(logLikelihoods, precision=2000L) {
-  library("Rmpfr")
+harmonicMean <- function(logLikelihoods, precision = 2000L) {
   llMed <- median(logLikelihoods)
   as.double(llMed - log(mean(exp(-mpfr(logLikelihoods,
                                        prec = precision) + llMed))))
 }
-burnin = 1000
-iter = 1000
-keep = 50
-ks <- seq(4, 100, by = 4)
+burnin <- 1000
+iter <- 1000
+keep <- 50
+ks <- seq(20, 40, by = 1)
 models <- lapply(ks, function(k) LDA(dtm, k, method = "Gibbs", control = list(burnin = burnin, iter = iter, keep = keep)))
 logLiks <- lapply(models, function(L)  L@logLiks[-c(1:(burnin/keep))])
 hm <- sapply(logLiks, function(h) harmonicMean(h))
+
 # Find optimal model
 plot(ks, hm, type = "l")
 opt <- models[which.max(hm)][[1]]
@@ -59,8 +68,7 @@ token.id <- opt@wordassignments$j
 topic.id <- opt@wordassignments$v
 vocab <- opt@terms
 
-# Get the phi matrix
-library(LDAviz)
+# Get the phi matrix using LDAviz
 dat <- getProbs(token.id, doc.id, topic.id, vocab, K = max(topic.id), sort.topics = "byTerms")
 phi <- t(dat$phi.hat)
 # NOTE TO SELF: these things have to be numeric vectors or else runVis() will break...add a check in check.inputs
@@ -68,12 +76,47 @@ token.frequency <- as.numeric(table(token.id))
 topic.id <- dat$topic.id
 topic.proportion <- as.numeric(table(topic.id)/length(topic.id))
 
-# Run the visualization locally
-library(LDAvis)
+# Run the visualization locally using LDAvis
 z <- check.inputs(K=max(topic.id), W=max(token.id), phi, token.frequency, vocab, topic.proportion)
 runVis()
 
-######### Not included in the post ###################
+
+
+
+
+
+
+
+
+
+
+
+
+
+###############################################################
+############ 'Verify' topics
+###############################################################
+
+#str(max.col(dat$theta.hat)) # This is equivalent to dat$main.topic (except the latter doesn't include prior weights) shouldn't this use reodered topics?
+theta <- dat$theta.hat[, dat$topic.order]
+colnames(theta) <- paste(1:dim(theta)[2])
+maxes <- apply(theta, 1, max)
+o <- order(maxes, decreasing = TRUE)
+maxes[o][1:5] # Top 5 'most distinguished' documents (e.g., most mass on a single topic) 
+w.max <- apply(theta, 1, which.max)
+w.max[o][1:5] # The topic responsible for that mass
+
+# Check that order of abstracts and subject areas match
+stopifnot(all(names(abs) == names(areas)))
+areas[o][1:5] # Subject areas for top 5
+abs[o][1:5]
+
+
+###############################################################
+###########       Track topics over time --        ############
+########### I didn't find anything that interesting ###########
+###############################################################
+
 # Track the prevalence of topics over time
 datez <- gsub(",", "", gsub(" ", "", tolower(dates)))
 date.id <- as.Date(datez, format = "%B%d%Y")[doc.id]
